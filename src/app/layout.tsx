841f081164944a07639d64d030b931c4407c9bd5
106xayt <arstedist@gmail.com>
2026-07-15 03:08:21 +0200

    Add Vercel Analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import type { ReactNode } from "react";
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/next";
 
 export const metadata: Metadata = {
   metadataBase: new URL("https://www.motion4frames.no"),
@@ -50,7 +51,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        {children}
+        <Analytics/>
+        </body>
     </html>
   );
 }
